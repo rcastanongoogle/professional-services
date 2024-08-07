0d5e5e232cd673c4946650132df5be1d72c18718
lourdesrojas@google.com <lourdesrojas@google.com>
2024-08-07 16:20:18 +0000

    Organizacion folders y sharedflows disponibles

--- examples/apigee-cicd-maven-plugin/RULE-ENGINE-API_rev96_2024_07_18/apiproxy/resources/py/dynamicBodyBase64.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-ruleSetId = flow.getVariable("ruleset.id")
-
-body = "{\"argument\":\"{\\\"body\\\":{\\\"ruleSetId\\\":\\\"%s\\\"},\\\"callLogLevel\\\":\\\"LOG_ALL_CALLS\\\"}\"}"%(ruleSetId)
-
-encoded = base64.b64encode(body)
-
-flow.setVariable("ruleset.body", encoded)
